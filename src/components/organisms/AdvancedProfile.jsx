bbf847745068d5fed3480b2fc65793e26597e984
satria_herman <satriaherman967@gmail.com>
2021-07-09 00:27:25 +0700

    [CREATE] sign up page

--- src/components/organisms/AdvancedProfile.jsx
+++ src/components/organisms/AdvancedProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ArrowLeftImg } from '@components/PathImage'
 import { Row , Col , Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import InputText from '@components/atoms/InputText'
 import ButtonRed from '@components/atoms/ButtonRed'
 
